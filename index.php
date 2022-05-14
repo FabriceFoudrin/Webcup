792a364ff263f495516ad2b1756f212a46598baf
FabriceFoudrin <fabricrfoudrin@gmail.com>
2022-05-14 13:29:29 +0400

    init project

---
+++ index.php
@@ -0,0 +1,39 @@
+<!doctype html>
+<html lang="fr">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="./inc/css/styles.css" rel="stylesheet">
+    <link href="./inc/css/bootstrap/bootstrap.min.css" rel="stylesheet">
+
+    <title>Hello, world!</title>
+  </head>
+  <body>
+    <div class="container">
+        <header>
+        
+    
+        </header>
+        <section></section>
+        <section></section>
+        <section></section>
+        <section></section>
+        <section></section>
+        <section></section>
+        <section></section>
+        <section></section>
+        <section></section>
+        <footer></footer>
+        <div id="popub_id"></div>
+    </div>
+    <h1>Hello, world!</h1>
+
+
+    <script src="./inc/js/bootstrap/bootstrap.bundle.min.js"></script>
+
+
+  </body>
+</html>
